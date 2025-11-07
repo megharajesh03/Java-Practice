bc4b1028b5bb8e84e18a873a3d2084c1c590c431
megharajesh03 <94694280+megharajesh03@users.noreply.github.com>
2025-11-07 14:45:35 +0530

    Delete Day1-8/DemoProject directory

--- Day1-8/DemoProject/Demo.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class Demo {
-	
-	public static void main(String[] args) {
-		System.out.println("Welcome to Java Programming");
-	}
-
-}
--- Day1-8/DemoProject/Ex10.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class Ex10 {
-	public static void main(String[] args) {
-		System.out.println(args[1]);
-	}
-}
